579c625c73eb9a4a91d746c8ffbf2a5ed0143fcc
robinvasquez <rvasquezb2@miumg.edu.gt>
2023-07-16 14:47:03 -0600

    cambio de puerto

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 80;
 const productController = require('./controllers/productController');
 
 app.use(express.json());
